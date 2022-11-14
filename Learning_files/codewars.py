fd97f4ce458c87dc45f9421ec193dc3aa0f69dd6
Felix5152 <116724120+Felix5152@users.noreply.github.com>
2022-11-14 19:34:57 +0100

    Added few kata's from codewars

--- Learning_files/codewars.py
+++ Learning_files/codewars.py
@@ -39,3 +39,29 @@ def grow(arr):
     for num in arr:
         total *= num
     return(total)
+
+
+#Find the next perfect square!
+import math
+def find_next_square(sq):
+    sq1 = math.sqrt(sq)
+    if sq % sq1 == 0:  
+        return ((sq1 + 1) ** 2)
+    elif sq % sq1 != 0:
+        return (-1)
+
+
+#2 bullets in order to kill 1 dragon
+def hero(bullets, dragons):
+    if dragons * 2 <= bullets:
+        return True
+    else:
+        return False
+
+
+#Miliseconds in given hour
+def past(h, m, s):
+    h1 = 3600000 #Amount of miliseconds in certain unit 
+    m1 = 60000
+    s1 = 1000
+    return(h * h1 + m * m1 + s * s1) #Counting total amount of miliseconds in certain hour.
